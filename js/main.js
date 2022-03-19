f0db1536b7f6eba825f9916ac5236dc08fab0626
Vams <piskorz.seb@gmail.com>
2022-03-19 16:16:57 +0100

    Update files

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 /*
 1. Ogarnać klasy według metodyki BEM
 2. "Skrócić" kod JS na bardziej wydajny i przejrzysty
-3. Komunikat z błędem
-4. Po odświeżeniu strony (f5, ikonka refresh, ctrl + f5 itp.) nie jest usuwana zawartość listy jedynie usuwa się po kliknieciu przycisku na dole listy 
+3. Po odświeżeniu strony (f5, ikonka refresh, ctrl + f5 itp.) nie jest usuwana zawartość listy jedynie usuwa się po kliknieciu przycisku na dole listy
+4. Naprawić Gita: remote: Support for password authentication was removed on August 13, 2021. Please use a personal access token instead.
++ wrzucić kod na githuba
+5. Ikony kosza obok dodanego elementu aby usunać pojedyńczo wybraną rzecz
+6. Przenieść pliki z branch master na main
+7. Dodawanie produktów przy pomocy entera
 */
 const buttonAdd = document.querySelector(".button__add");
 const textList = document.querySelector("input");
 const buttonResetAll = document.querySelector(".footer__button");
+const trashIcon = document.querySelector("svg");
+const error = document.querySelector("form__error-message");
 
 buttonAdd.addEventListener("click", function addElement() {
     if (textList.value.length < 3 || textList.value.length > 25) {
-        console.log("Niepoprawna liczba znaków (minimum 3, maksymalnie)");
+        document.getElementById("form__error-message").innerHTML = "Niepoprawna liczba znaków (minimum 3, maksymalnie 25)";
         textList.value = "";
     } else {
         const text = textList.value;
@@ -19,8 +25,20 @@ buttonAdd.addEventListener("click", function addElement() {
         const node = document.createTextNode(text);
         pText.appendChild(node);
         document.getElementById("card__list").appendChild(pText);
+        document.getElementById("form__error-message").innerHTML = ""; /*?*/
     }
 });
+
+function keepContent() {
+    var someVarName = localStorage.getItem(card__list);
+    console.log(someVarName)
+}cccccccc
+
+
+buttonAdd.addEventListener("click", function addIcon() {
+    //document.getElementById("card__list").classList.add("show");
+});
+
 
 buttonResetAll.addEventListener("click", function clearAll() {
     document.getElementById("card__list").innerHTML = "";
