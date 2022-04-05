f7c746f450665cc5c53cfdbcfd4ef9281d586b10
Vams <piskorz.seb@gmail.com>
2022-04-05 16:52:35 +0200

    Add new features

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-const buttonAdd = document.querySelector(".button__add");
+const buttonAdd = document.querySelector(".form__add");
 const textList = document.querySelector("input");
 const buttonResetAll = document.querySelector(".footer__button");
+const addIcon = document.querySelectorAll(".card__icon");
 
 buttonAdd.addEventListener("click", function addElement() {
     if (textList.value.length < 3 || textList.value.length > 25) {
@@ -13,21 +14,11 @@ buttonAdd.addEventListener("click", function addElement() {
         const pText = document.createElement("p");
         const node = document.createTextNode(text);
         pText.appendChild(node);
-        document.getElementById("card__list").appendChild(pText);
-    
+        const finalContent = document.getElementById("card__list").appendChild(pText);
+        addIcon.forEach(iconStyle => iconStyle.style.display = "inline-block");
+        finalContent.appendChild(addIcon[0]);
     }
 });
-
-/*function keepContent() {
-    var someVarName = localStorage.getItem(card__list);
-    console.log(someVarName)
-};
-
-
-buttonAdd.addEventListener("click", function addIcon() {
-    //document.getElementById("card__list").classList.add("show");
-});*/
-
 
 buttonResetAll.addEventListener("click", function clearAll() {
     document.getElementById("card__list").innerHTML = "";
