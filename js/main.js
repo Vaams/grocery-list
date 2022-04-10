bd1d824ecd625709f38ccc28fc9003f1ac9a0eba
Vams <piskorz.seb@gmail.com>
2022-04-10 19:12:01 +0200

    Add new features and code refactoring

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 const buttonAdd = document.querySelector(".form__add");
 const textList = document.querySelector("input");
 const buttonResetAll = document.querySelector(".footer__button");
-const addIcon = document.querySelectorAll(".card__icon");
 
 buttonAdd.addEventListener("click", function addElement() {
     if (textList.value.length < 3 || textList.value.length > 25) {
@@ -14,9 +13,12 @@ buttonAdd.addEventListener("click", function addElement() {
         const pText = document.createElement("p");
         const node = document.createTextNode(text);
         pText.appendChild(node);
-        const finalContent = document.getElementById("card__list").appendChild(pText);
-        addIcon.forEach(iconStyle => iconStyle.style.display = "inline-block");
-        finalContent.appendChild(addIcon[0]);
+        const finalContent = document.getElementById("card__list");
+        const icons = '<i class="fa-solid fa-trash-can card__icon"></i>' + '<i class="fa-solid fa-pen-to-square card__icon"></i>';
+        finalContent.innerHTML += `<p>${text}` + `${icons}</p>`;
+        for (let i = 1; i < finalContent.childNodes.length; i++) {
+            finalContent.insertBefore(finalContent.childNodes[i], finalContent.firstChild);
+        }
     }
 });
 
